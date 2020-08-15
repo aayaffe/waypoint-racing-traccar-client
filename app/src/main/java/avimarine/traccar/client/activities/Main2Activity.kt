d6cd48b9505bb40fb769fdf1636140c79dda5507
Amit Y <aayaffe@gmail.com>
2020-08-15 08:33:40 +0300

    Register for json files, and save to local storage for route loading

--- app/src/main/java/avimarine/traccar/client/activities/Main2Activity.kt
+++ app/src/main/java/avimarine/traccar/client/activities/Main2Activity.kt
@@ -1,12 +1,15 @@
 package avimarine.traccar.client.activities
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Location
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.OpenableColumns
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -22,6 +25,8 @@ import avimarine.traccar.client.route.*
 import avimarine.traccar.client.ui.RouteElementAdapter
 import avimarine.traccar.client.utils.Screenshot
 import kotlinx.android.synthetic.main.activity_main2.*
+import java.io.FileInputStream
+import java.io.FileNotFoundException
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.schedule
@@ -31,19 +36,18 @@ class Main2Activity : AppCompatActivity(), PositionListener, SharedPreferences.O
 
     private val magnetic = false
     private lateinit var positionProvider: PositionProvider
-    private var nextWpt : RouteElement? = null
+    private var nextWpt: RouteElement? = null
     private lateinit var sharedPreferences: SharedPreferences
     private val PERMISSIONS_REQUEST_LOCATION = 2
-    private lateinit var route : Route
+    private lateinit var route: Route
+    private val MAXFILESIZE = 10000
+    private val DEFAULTFILENAME = "myRoute"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         sharedPreferences = getDefaultSharedPreferences(this)
-        val text = resources.openRawResource(R.raw.test)
-                .bufferedReader().use { it.readText() }
-        route = Route.fromGeoJson(text)
-        populateRouteElementSpinner(route)
+        initRoute()
         positionProvider = PositionProviderFactory.create(this, this)
         setButton(sharedPreferences.getBoolean(MainFragment.KEY_STATUS, false))
         if (sharedPreferences.getBoolean(MainFragment.KEY_STATUS, false)) {
@@ -52,22 +56,124 @@ class Main2Activity : AppCompatActivity(), PositionListener, SharedPreferences.O
         val mCalendar: Calendar = GregorianCalendar()
         val mTimeZone = mCalendar.timeZone
         val mGMTOffset = mTimeZone.getOffset(mCalendar.timeInMillis)
-        time.setLabel("UTC " + (if (mGMTOffset>0) "+" else "") + TimeUnit.HOURS.convert(mGMTOffset.toLong(), TimeUnit.MILLISECONDS))
+        time.setLabel("UTC " + (if (mGMTOffset > 0) "+" else "") + TimeUnit.HOURS.convert(mGMTOffset.toLong(), TimeUnit.MILLISECONDS))
         routeElementSpinner.onItemSelectedListener = object :
                 AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>,
                                         view: View, position: Int, id: Long) {
                 nextWpt = route.elements[position]
-                if (nextWpt!!.firstTimeInProofArea!=-1L){
+                if (nextWpt!!.firstTimeInProofArea != -1L) {
                     (parent.getChildAt(0) as TextView).setTextColor(resources.getColor(android.R.color.holo_green_light))
                 } else {
                     (parent.getChildAt(0) as TextView).setTextColor(resources.getColor(android.R.color.black))
                 }
             }
+
             override fun onNothingSelected(parent: AdapterView<*>) {
                 // write code to perform some action
             }
         }
+    }
+
+    private fun initRoute() {
+        when {
+            intent?.action == Intent.ACTION_SEND -> {
+                if ("text/json" == intent.type || "application/json" == intent.type) {
+                    val s = handleJsonText(intent) // Handle json being sent
+                    this.openFileOutput(DEFAULTFILENAME, Context.MODE_PRIVATE).use {
+                        it.write(s.toByteArray())
+                    }
+                    route = Route.fromGeoJson(s)
+                } else {
+                    if (checkIntent(intent)) {
+                        val s = handleJsonText(intent) // Handle json being sent
+                        this.openFileOutput(DEFAULTFILENAME, Context.MODE_PRIVATE).use {
+                            it.write(s.toByteArray())
+                        }
+                        route = Route.fromGeoJson(s)
+                    }
+                    Log.d(TAG, "Unknown intent type: " + intent?.type)
+                }
+            }
+            else -> {
+                Log.d(TAG, "Unknown intent action: " + intent?.action)
+            }
+        }
+
+        if (!::route.isInitialized) {
+            try {
+                val text = this.openFileInput(DEFAULTFILENAME).bufferedReader().use { it.readText() }
+                route = Route.fromGeoJson(text)
+            } catch (e: FileNotFoundException) {
+                Log.d(TAG, "file not found", e)
+            }
+
+        }
+        if (::route.isInitialized)
+            populateRouteElementSpinner(route)
+    }
+
+    private fun checkIntent(intent: Intent): Boolean {
+        val uri: Uri
+        intent.clipData?.let { clipData ->
+            Log.d(TAG, clipData.toString())
+            uri = clipData.getItemAt(0).uri
+            contentResolver.query(uri, null, null, null, null)?.use { cursor ->
+                cursor.moveToFirst()
+                val name = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                val size = cursor.getLong(cursor.getColumnIndex(OpenableColumns.SIZE))
+                Log.d(TAG, "Json filesize: " + size)
+                Log.d(TAG, "Json filename: " + name)
+                return if ((size < MAXFILESIZE) && ("json" in name)) {
+                    true
+                } else {
+                    Log.d(TAG, "JSON File too big, or not contains json. size: $size name: $name")
+                    //TODO Check for proper file limit and add Toast
+                    false
+                }
+            }
+        }
+        return false
+    }
+
+    /**
+     * Returns text value of intent, empty string if error.
+     *
+     * @param intent
+     * @return text value of intent, empty string if error.
+     */
+    private fun handleJsonText(intent: Intent): String {
+        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+            //Handle Simpleshare
+            return it
+        }
+        //Handle shared file share
+        val uri = getUri(intent)
+        if (uri!=null) {
+            if (checkIntent(intent)){
+                val inputPFD = try {
+                    contentResolver.openFileDescriptor(uri, "r")
+                } catch (e: FileNotFoundException) {
+                    Log.e("MainActivity", "File not found.", e)
+                    return ""
+                }
+                // Get a regular file descriptor for the file
+                val fd = inputPFD?.fileDescriptor
+                val fileStream = FileInputStream(fd)
+                return fileStream.bufferedReader().use { it.readText() }
+            } else {
+                return ""
+            }
+        }
+        return ""
+    }
+
+    private fun getUri(intent: Intent): Uri? {
+        intent.clipData?.let { clipData ->
+            Log.d(TAG, clipData.toString())
+            return clipData.getItemAt(0).uri
+        }
+        return null
     }
 
     override fun onStart() {
@@ -78,14 +184,17 @@ class Main2Activity : AppCompatActivity(), PositionListener, SharedPreferences.O
             Log.w(TAG, e)
         }
     }
+
     override fun onResume() {
         super.onResume()
         sharedPreferences.registerOnSharedPreferenceChangeListener(this)
     }
+
     override fun onPause() {
         super.onPause()
         sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
     }
+
     override fun onStop() {
         try {
             positionProvider.stopUpdates()
@@ -115,20 +224,20 @@ class Main2Activity : AppCompatActivity(), PositionListener, SharedPreferences.O
         finishEast.latitude = 32.8371666666667
         finishEast.longitude = 35.02455
         val finishWest = Location("")
-        finishWest.latitude =  32.8371666666667
+        finishWest.latitude = 32.8371666666667
         finishWest.longitude = 35.0200666666667
 
-        val acco = Gate("Acco Gate", accoEast,accoWest,true,315.0,45.0, 0.25)
-        val nahariya = Gate("Nahariya Gate", nahariyaEast,nahariyaWest,true,315.0,45.0, 0.25)
-        val achzivWpt = Waypoint("Achziv Turning Point", achziv, true,315.0,45.0)
+        val acco = Gate("Acco Gate", accoEast, accoWest, true, 315.0, 45.0, 0.25)
+        val nahariya = Gate("Nahariya Gate", nahariyaEast, nahariyaWest, true, 315.0, 45.0, 0.25)
+        val achzivWpt = Waypoint("Achziv Turning Point", achziv, true, 315.0, 45.0)
         val finish = Finish("Finish Line", finishWest, finishEast, 0.15)
-        return Route("TestEvent", Calendar.getInstance().time, arrayListOf(acco,nahariya,achzivWpt,finish),Calendar.getInstance().time)
+        return Route("TestEvent", Calendar.getInstance().time, arrayListOf(acco, nahariya, achzivWpt, finish), Calendar.getInstance().time)
 
     }
 
-    fun populateRouteElementSpinner(route: Route){
+    fun populateRouteElementSpinner(route: Route) {
         val adapter = RouteElementAdapter(this,
-                R.layout.waypoint_spinner_item,0, route.elements)
+                R.layout.waypoint_spinner_item, 0, route.elements)
         routeElementSpinner.adapter = adapter
     }
 
@@ -149,8 +258,7 @@ class Main2Activity : AppCompatActivity(), PositionListener, SharedPreferences.O
             val intent = Intent(this, StatusActivity::class.java)
             this.startActivity(intent)
             return true
-        }
-        else if (id == R.id.send_screenshot_menu_action){
+        } else if (id == R.id.send_screenshot_menu_action) {
             val b = Screenshot.takescreenshotOfRootView(this.findViewById<View>(android.R.id.content).rootView)
 
         }
@@ -160,7 +268,7 @@ class Main2Activity : AppCompatActivity(), PositionListener, SharedPreferences.O
     fun startButtonClick(view: View) {
         val sharedPref: SharedPreferences = getDefaultSharedPreferences(this)
         val checked = sharedPref.getBoolean("status", true)
-        with (sharedPref.edit()) {
+        with(sharedPref.edit()) {
             putBoolean("status", checked.not())
             commit()
         }
@@ -175,25 +283,25 @@ class Main2Activity : AppCompatActivity(), PositionListener, SharedPreferences.O
 //        StatusActivity.addMessage(context.getString(R.string.status_location_update))
         if (position != null) {
             updateUI(position)
-        } else{
+        } else {
             Log.w(TAG, "Error: position is null")
         }
 
     }
 
     private fun updateUI(position: Position) {
-        if (nextWpt!=null) {
+        if (nextWpt != null) {
             val distWptPort = getDistance(position, nextWpt!!.portWpt)
             val distWptStbd = getDistance(position, nextWpt!!.stbdWpt)
             val dirWptPort = getDirection(position, nextWpt!!.portWpt)
             val dirWptStbd = getDirection(position, nextWpt!!.stbdWpt)
-            val portData = getDirString(dirWptPort,magnetic,false,position,position.time.time) + "/" + getDistString(distWptPort)
-            val stbcData = getDirString(dirWptStbd,magnetic,false,position,position.time.time) + "/" + getDistString(distWptStbd)
+            val portData = getDirString(dirWptPort, magnetic, false, position, position.time.time) + "/" + getDistString(distWptPort)
+            val stbcData = getDirString(dirWptStbd, magnetic, false, position, position.time.time) + "/" + getDistString(distWptStbd)
             portGate.setData(portData)
             stbdGate.setData(stbcData)
             updateIsInArea(position)
         }
-        cog.setData(getDirString(position.course,magnetic,false,position,position.time.time))
+        cog.setData(getDirString(position.course, magnetic, false, position, position.time.time))
         sog.setData(getSpeedString(position.speed)) //TODO Fix units in the conversion (probably knots)
         location.setData(getLatString(position.latitude) + "\n" + getLonString(position.longitude))
         time.setData(timeStamptoDateString(position.time.time))
@@ -205,14 +313,14 @@ class Main2Activity : AppCompatActivity(), PositionListener, SharedPreferences.O
         val l = Location("")
         l.latitude = location.latitude
         l.longitude = location.longitude
-        if (nextWpt!=null){
-            if (nextWpt!!.isInProofArea(l)){
-                if (nextWpt!!.passedGate(location)){
+        if (nextWpt != null) {
+            if (nextWpt!!.isInProofArea(l)) {
+                if (nextWpt!!.passedGate(location)) {
                     StatusActivity.addMessage("Passed " + nextWpt!!.name)
                     (routeElementSpinner.selectedView as TextView).setTextColor(resources.getColor(android.R.color.holo_green_light))
                     Timer("AdvanceWaypoint", false).schedule(3000) {
                         runOnUiThread {
-                            if (routeElementSpinner.selectedItemPosition < routeElementSpinner.adapter.count-1) {
+                            if (routeElementSpinner.selectedItemPosition < routeElementSpinner.adapter.count - 1) {
                                 routeElementSpinner.setSelection(routeElementSpinner.selectedItemPosition + 1)
                             }
                         }
@@ -236,7 +344,7 @@ class Main2Activity : AppCompatActivity(), PositionListener, SharedPreferences.O
     }
 
     private fun setButton(isRunning: Boolean) {
-        if (isRunning){
+        if (isRunning) {
             start_btn.background = ContextCompat.getDrawable(this, R.drawable.btn_rnd_red)
             start_btn.text = "Stop"
         } else {
